d7d3a6fec18f3c235eba4d33925f803996e06a51
haseebullahabbasi <haseebabbasi00@gmail.com>
2023-08-22 19:09:43 +0500

    Preparing for v1

---
+++ src/Evergreen/V1/Types.elm
@@ -0,0 +1,39 @@
+module Evergreen.V1.Types exposing (..)
+
+import Lamdera
+
+
+type MessageType
+    = Joined Lamdera.ClientId
+    | Left Lamdera.ClientId
+    | Message Lamdera.ClientId String
+
+
+type alias FrontendModel =
+    { colors : List MessageType
+    , messageFieldContent : String
+    }
+
+
+type alias BackendModel =
+    { colors : List MessageType
+    }
+
+
+type FrontendMsg
+    = SendColor String
+    | Noop
+
+
+type ToBackend
+    = MsgSubmitted String
+
+
+type BackendMsg
+    = ClientConnected Lamdera.SessionId Lamdera.ClientId
+    | ClientDisconnected Lamdera.SessionId Lamdera.ClientId
+
+
+type ToFrontend
+    = HistoryReceived (List MessageType)
+    | MessageReceived MessageType
